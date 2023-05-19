51f2d5e25174efa3d44b4a2be4ab2e8c350da507
CAMgalaxy <86727349+CAMgalaxy@users.noreply.github.com>
2023-05-19 16:42:52 +0200

    Add files via upload

--- config/defaultCAM.js
+++ config/defaultCAM.js
@@ -1,4 +1,4 @@
-/* default CAM which will be redrawn if CAM is deleted */
+/* default CAM which will be redrawn if CAM is deleted
 function shuffle(queslist) {
     let array_emp = [];
     for (var i = 0; i < queslist.length; i++) {
@@ -15,55 +15,42 @@ function shuffle(queslist) {
     return array_emp;
   }
 
-const ConceptsCAM = ["positive feelings", "negative feelings", 
-"trust in policitical institutions",
-"perceived risks", "perceived benefits"];
+const ConceptsCAM = ["Vorteile", "Nachteile"];
 
 const index_ConceptsCAM = shuffle(ConceptsCAM);
 console.log("index_ConceptsCAM:", index_ConceptsCAM);
-
+ */
 
 function defaultCAM() {
-    CAM.addElement(new NodeCAM(1, "I cannot be moved or deleted", {
-        x: 300,
-        y: 100
-    }, false, false, true));
+    CAM.addElement(new NodeCAM(0, "default CAM", {
+        x: 600,
+        y: 400
+    }, false, false, false));
 
-    CAM.addElement(new NodeCAM(-1, "I cannot be moved but deleted", {
-        x: 300,
-        y: 700
-    }, false, true, true));
-
-    CAM.addElement(new NodeCAM(0, "Neutral concept (cannot change text)", {
-        x: 1000,
-        y: 100
-    }, true, true, false));
-
-    CAM.addElement(new NodeCAM(10, "Ambivalent concept", {
-        x: 1000,
-        y: 700
-    }, true, true, true));
+    /*
+    CAM.addElement(new NodeCAM(0, ConceptsCAM[index_ConceptsCAM[0]], {
+      x: 450,
+      y: 400
+  }, false, false, false));
 
 
+  CAM.addElement(new NodeCAM(0, ConceptsCAM[index_ConceptsCAM[1]], {
+    x: 750,
+    y: 400
+}, false, false, false));
 
 
-    var connector1 = new ConnectorCAM();
-    connector1.establishConnection(CAM.nodes[0], CAM.nodes[1], IncreaseSliderIntensity, false);
-    connector1.value = 1;
-    CAM.addElement(connector1);
+var connector = new ConnectorCAM();
+connector.establishConnection(CAM.nodes[0], CAM.nodes[1], IncreaseSliderIntensity, true);
+CAM.addElement(connector);
+CAM.connectors[0].isDeletable = false;
 
-    var connector2 = new ConnectorCAM();
-    connector2.establishConnection(CAM.nodes[0], CAM.nodes[2], IncreaseSliderIntensity, true);
-    connector2.value = 1;
-    CAM.addElement(connector2);
-
-    var connector3 = new ConnectorCAM();
-    connector3.establishConnection(CAM.nodes[0], CAM.nodes[3], IncreaseSliderIntensity, true);
-    connector3.value = 1;
-    CAM.addElement(connector3);
-    connector3.isBidirectional = false;
+var connector = new ConnectorCAM();
+connector.establishConnection(CAM.nodes[0], CAM.nodes[2], IncreaseSliderIntensity, true);
+CAM.addElement(connector);
+CAM.connectors[1].isDeletable = false;
+*/ 
 
 
-
-    CAM.draw();
+CAM.draw();
 }
