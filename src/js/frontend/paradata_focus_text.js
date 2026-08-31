a73730c9243f7302e83740ba8ef0ca8563f59265
Julius <fenn.statistics@posteo.de>
2026-08-31 09:32:05 +0200

    vite build

--- src/js/frontend/paradata_focus_text.js
+++
@@ -1,6 +0,0 @@
-// language file
-$(function () {
-    document.getElementById("alert-text").innerHTML = languageFileOut.ls_01; // leaving fullscreen
-    
-    document.getElementById("alert-button-text").innerText =   languageFileOut.continueButtonFullscreen; // fullscreen continue button
-});
